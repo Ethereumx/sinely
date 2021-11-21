7479f8c5a2c1fa98a97a2005dd9ff0f6dd53a142
drging <i.touailaba.7@gmail.com>
2021-11-21 19:02:35 +0100

    update sign file to receive file as param

--- features/sign/commands/sign-file.js
+++ features/sign/commands/sign-file.js
@@ -1,6 +1,7 @@
 const registerRepo = require('../repository');
 const { USER_NOT_FOUND_ERROR_MESSAGE,SIGNATURE_ERROR_MESSAGE,FILE_NOT_FOUND_ERROR_MESSAGE,SIGN_INFO_SUCCESS_MESSAGE } = require('../constants');
 const { getUser } = require('../repository');
+const path = require('path');
 
 async function signFile(req, res) {
   
@@ -10,6 +11,7 @@ async function signFile(req, res) {
     user: { id },
   } = req;
   try {
+  
     userInfo = await getUser(user && id);
   } catch (getUserError) {
     console.log(" USER_NOT_FOUND_ERROR_MESSAGE",getUserError)
@@ -30,9 +32,22 @@ async function signFile(req, res) {
 
     console.log('status file',statusFile)
     const getKey=await registerRepo.getPrivateKey(id);
-    file = await registerRepo.signFile(getKey.private_key,req.file.path,id,
+    if(req.body.path === undefined)
+    {
+      file = await registerRepo.signFile(getKey.private_key,req.file.path,id,
       req.file.originalname, req.body.description, statusFile,
-      req.user.public_key); 
+      req.user.public_key);
+    }else
+    {
+      const url = req.body.path;
+      const splitedPath = url.split('/');
+      const fileName = splitedPath[splitedPath.length-1];
+      const filePath = path.join(__dirname, '../../../uploads/'+fileName);
+      console.log('info ',filePath,fileName);
+      file = await registerRepo.signFile(getKey.private_key,filePath,id,
+        fileName, req.body.description, statusFile,
+        req.user.public_key);
+    }
   
   } catch (error) {
     console.log("ERROR", error)
